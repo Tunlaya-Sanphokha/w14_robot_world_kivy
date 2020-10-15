d54ddbd98eb134784813f2a3173d6d0c8228dcff
Pannawat Kingkaew <s6201012620163@email.kmutnb.ac.th>
2020-10-15 14:19:12 +0700

    change robot color& check position)

--- main.py
+++ main.py
@@ -1,40 +1,52 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.graphics import Rectangle
+from kivy.graphics import *
 from kivy.core.window import Window
 
-class Robot(Widget):
+class World(Widget):
+
+  position = [ [0 for i in range(10) ] for j in range(10) ]
+  
+  for i in range (10):
+    for j in range (10):
+      position[i][j] = 0
+      
   def __init__(self,**kwargs):
     super().__init__(**kwargs)
     self._keyboard = Window.request_keyboard(self._on_keyboard_closed,self)
     self._keyboard.bind(on_key_down = self._on_key_down)
 
     with self.canvas:
+      Color(250., 0, 0)
       self.robot = Rectangle(pos = (0,0) , size = (50,50))
 
+  
   def _on_keyboard_closed(self):
     self._keyboard.unbind(on_key_down = self._on_key_down)
     self._keyboard = None
-
+    
   def _on_key_down(self,keyboard,keycode,text,modifiers):
-    x = self.robot.pos[0]
-    y = self.robot.pos[1]
-  
-    if text == "w":
-      y += 50
-    if text == "s":
-      y -= 50
-    if text == "a":
-      x -= 50
-    if text == "d":
-      x += 50
+    row = self.robot.pos[1]
+    column = self.robot.pos[0]
 
-    self.robot.pos = (x,y)
+    if text == "w":
+      row += 50
+    if text == "s":
+      row -= 50
+    if text == "a":
+      column -= 50
+    if text == "d":
+      column += 50
+
+    self.robot.pos = (column,row)
+    
+    print(self.position)
 
 class RobotWorld(App):
   def build(self):
     Window.size = (500,500)
-    return Robot()
+    return World() 
 
 if __name__ == "__main__":
   RobotWorld().run()
+
